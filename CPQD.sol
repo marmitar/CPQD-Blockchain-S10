50090c7865abc31a7fa874cd15b18aaa87f46a0b
Tiago de Paula <tiagodepalves@gmail.com>
2025-05-30 19:24:42 -0300

    fix: limit commitment to the owner
    Fixes requirement 1

--- CPQD.sol
+++ CPQD.sol
@@ -3,12 +3,35 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 contract CPQD {
+    /**
+     * Address of this contract's creator.
+     *
+     * ## References
+     * - https://docs.soliditylang.org/en/latest/contracts.html#constant-and-immutable-state-variables
+     * - https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+     */
+    address public immutable owner = msg.sender;
 
     bytes32 hash;
     uint salt;
     mapping (uint => address []) public bets;
 
-    function commitment (bytes32 h, uint s) public {
+    /**
+     * @dev The caller account is not authorized to perform an operation.
+     */
+    error CPQDUnauthorizedAccount(address account);
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        if (owner != msg.sender) {
+            revert CPQDUnauthorizedAccount(msg.sender);
+        }
+        _;
+    }
+
+    function commitment(bytes32 h, uint s) public onlyOwner {
         hash = h;
         salt = s;
     }
